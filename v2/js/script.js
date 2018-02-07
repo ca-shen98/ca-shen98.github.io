defb04796268ab2c885e7df724b77f772b5e8af3
Carl Shen <carl.shen3@gmail.com>
2018-02-06 21:02:06 -0800

    Icon change

--- v2/js/script.js
+++ v2/js/script.js
@@ -4,17 +4,28 @@ var VALID_ANCHORS = [ 'aboutme', 'projects' ];
 function init() {
     window.onhashchange = function() {
         var newAnchor = location.hash;
-        if (newAnchor.length > 0) {
+        if(newAnchor.length > 0) {
             newAnchor = newAnchor.slice(1);
-            if (VALID_ANCHORS.indexOf(newAnchor) >= 0) {
-                // switch to newAnchor if not already there
-                _currentAnchor = newAnchor;
+            if(VALID_ANCHORS.indexOf(newAnchor) >= 0) {
+                if(_currentAnchor !== newAnchor) {
+                    switchAnchor(newAnchor);
+                }
             } else {
                 location.hash = _currentAnchor;
             }
         } else {
-            // switch to home if not already there
-            _currentAnchor = newAnchor;
+            if(_currentAnchor !== newAnchor) {
+                switchAnchor(newAnchor);
+            }
         }
     };
+}
+
+function switchAnchor(newAnchor) {
+    if(newAnchor.length == 0) {
+
+    } else {
+        
+    }
+    _currentAnchor = newAnchor;
 }
