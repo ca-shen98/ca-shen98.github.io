b1b2eb40ad8f54da10865365e4e8265c740bcb10
Carl Shen <carl.shen3@gmail.com>
2018-02-07 08:52:42 -0800

    Added landing button
    - not mobile friendly yet

--- v2/js/script.js
+++ v2/js/script.js
@@ -7,25 +7,24 @@ function init() {
         if(newAnchor.length > 0) {
             newAnchor = newAnchor.slice(1);
             if(VALID_ANCHORS.indexOf(newAnchor) >= 0) {
-                if(_currentAnchor !== newAnchor) {
-                    switchAnchor(newAnchor);
-                }
-            } else {
-                location.hash = _currentAnchor;
-            }
-        } else {
-            if(_currentAnchor !== newAnchor) {
                 switchAnchor(newAnchor);
-            }
+            } // else
+        } else {
+            switchAnchor(newAnchor);
         }
     };
 }
 
 function switchAnchor(newAnchor) {
-    if(newAnchor.length == 0) {
-
-    } else {
-        
-    }
+    if(VALID_ANCHORS.indexOf(_currentAnchor) >= 0) {
+        $('#' + _currentAnchor + '_nav-item').removeClass('current-item');
+    } else if(_currentAnchor.length == 0) {
+        $('#landing_nav-item').removeClass('current-item');
+    } // else
+    if(VALID_ANCHORS.indexOf(newAnchor) >= 0) {
+        $('#' + newAnchor + '_nav-item').addClass('current-item');
+    } else if(newAnchor.length == 0) {
+        $('#landing_nav-item').addClass('current-item');
+    } // else
     _currentAnchor = newAnchor;
 }
