c6587252edd7420446d7efafcda519a11a6155ef
Carl Shen <carl.shen3@gmail.com>
2018-02-08 08:53:14 -0800

    Animate the nav menu

--- v2/js/script.js
+++ v2/js/script.js
@@ -1,30 +1,34 @@
 var _currentAnchor = '';
-var VALID_ANCHORS = [ 'aboutme', 'projects' ];
+var VALID_ANCHORS = [ '', 'aboutme', 'projects' ];
 
 function init() {
     window.onhashchange = function() {
         var newAnchor = location.hash;
         if(newAnchor.length > 0) {
             newAnchor = newAnchor.slice(1);
-            if(VALID_ANCHORS.indexOf(newAnchor) >= 0) {
-                switchAnchor(newAnchor);
-            } // else
-        } else {
+        }
+        if(VALID_ANCHORS.indexOf(newAnchor) >= 0) {
             switchAnchor(newAnchor);
+        } else {
+            location.hash = _currentAnchor;
         }
     };
 }
 
 function switchAnchor(newAnchor) {
-    if(VALID_ANCHORS.indexOf(_currentAnchor) >= 0) {
-        $('#' + _currentAnchor + '_nav-item').removeClass('current-item', 200);
-    } else if(_currentAnchor.length == 0) {
-        $('#landing_nav-item').removeClass('current-item', 100);
-    } // else
-    if(VALID_ANCHORS.indexOf(newAnchor) >= 0) {
-        $('#' + newAnchor + '_nav-item').addClass('current-item', 200);
-    } else if(newAnchor.length == 0) {
-        $('#landing_nav-item').addClass('current-item', 100);
-    } // else
+    $('#' + _currentAnchor + '_nav-item').removeClass('current-item', 200);
+    $('#' + newAnchor + '_nav-item').addClass('current-item', 200);
+    if(!isLandingAnchor(newAnchor) && isLandingAnchor(_currentAnchor)) {
+        $('#nav-wrapper').css('bottom', $('#page-wrapper').height() - $('#nav-wrapper').offset().top - $('#nav-wrapper').outerHeight(true));
+        $('#nav-wrapper').css('top', '');
+        $('#nav-wrapper').animate({ bottom: 0 }, 200);
+    } else if(isLandingAnchor(newAnchor)) {
+        $('#nav-wrapper').animate({ top: 0 }, 200);
+        $('#nav-wrapper').css('bottom', '');
+    }
     _currentAnchor = newAnchor;
+}
+
+function isLandingAnchor(anchor) {
+    return anchor.length > 0;
 }
