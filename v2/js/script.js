35fe6b4e42466e48daf6bb55f0930a7f7d61934b
Carl Shen <carl.shen3@gmail.com>
2018-02-08 22:40:16 -0800

    Animated home button too

--- v2/js/script.js
+++ v2/js/script.js
@@ -13,18 +13,53 @@ function init() {
             location.hash = _currentAnchor;
         }
     };
+    var tempAnchor = location.hash;
+    if(tempAnchor.length > 0) {
+        tempAnchor = tempAnchor.slice(1);
+    }
+    if(VALID_ANCHORS.indexOf(tempAnchor) >= 0) {
+        switchAnchor(tempAnchor);
+    } else {
+        location.hash = '';
+    }
 }
 
-function switchAnchor(newAnchor) {
-    $('#' + _currentAnchor + '_nav-item').removeClass('current-item', 200);
-    $('#' + newAnchor + '_nav-item').addClass('current-item', 200);
+function switchAnchor(newAnchor, animate=true) {
+    $('#' + _currentAnchor + '_nav-item').removeClass('current-item', animate ? 250 : 0);
+    $('#' + newAnchor + '_nav-item').addClass('current-item', animate ? 250 : 0);
     if(!isLandingAnchor(newAnchor) && isLandingAnchor(_currentAnchor)) {
+        $('#nav-row').css('border-top', '1px solid darkgreen');
+        $('#nav-row').css('padding-top', '5px');
+
+        $('#_nav-item').css('bottom', $('#page-wrapper').height() - $('#_nav-item').offset().top - $('#_nav-item').outerHeight(true));
         $('#nav-wrapper').css('bottom', $('#page-wrapper').height() - $('#nav-wrapper').offset().top - $('#nav-wrapper').outerHeight(true));
+
+        $('#_nav-item').css('top', '');
         $('#nav-wrapper').css('top', '');
-        $('#nav-wrapper').animate({ bottom: 0 }, 200);
+        if(animate) {
+            $('#_nav-item').animate({ bottom: 0 }, { duration: 250, queue: false });
+            $('#nav-wrapper').animate({ bottom: 0 }, { duration: 250, queue: false });
+        } else {
+            $('#_nav-item').css('bottom', 0);
+            $('#nav-wrapper').css('bottom', 0);
+        }
+
+        $('#nav-row').css('border-bottom', '0px');
+        $('#nav-row').css('padding-bottom', '10px');
     } else if(isLandingAnchor(newAnchor)) {
-        $('#nav-wrapper').animate({ top: 0 }, 200);
-        $('#nav-wrapper').css('bottom', '');
+        $('#nav-row').css('border-bottom', '1px solid darkgreen');
+        $('#nav-row').css('padding-bottom', '5px');
+
+        if(animate) {
+            $('#_nav-item').animate({ top: 0 }, { duration: 250, queue: false });
+            $('#nav-wrapper').animate({ top: 0 }, { duration: 250, queue: false });
+        } else {
+            $('#_nav-item').css('top', 0);
+            $('#nav-wrapper').css('top', 0);
+        }
+
+        $('#nav-row').css('border-top', '0px');
+        $('#nav-row').css('padding-top', '10px');
     }
     _currentAnchor = newAnchor;
 }
